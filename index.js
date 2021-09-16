6ffd36850dcbb3d05904cb965544ee28df715bf3
adam-belahouane <adam_belahouane@outlook.com>
2021-09-16 15:58:36 +0100

    update

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ fetchapi = () => {
     .then((response) => {
         return response.json()})
     .then((body) =>{
-        console.log(body)
         let row = document.querySelector(".row")
         body.forEach((body) => {
-            console.log(body)
             let img = body.img
             let col = document.createElement('div')
             col.classList = 'col-md-3'
@@ -22,10 +20,10 @@ fetchapi = () => {
               </p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" onclick="addToCart(event)">
                     Add to cart
                   </button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" onclick="skipBook(event)">
                     skip
                   </button>
                 </div>
@@ -37,4 +35,45 @@ fetchapi = () => {
         })
 
     })
+}
+skipBook = (event) => {
+    event.currentTarget.closest(".col-md-3").remove()
+}
+addToCart = (event) => {
+    console.log(event)
+    console.log(event.path[3].childNodes[1].innerText)
+    console.log(event.path[4].childNodes[1].currentSrc)
+    let img = event.path[4].childNodes[1].currentSrc
+    let imgTitle = event.path[3].childNodes[1].innerText
+
+    let Scart = document.querySelector("#shopping-cart")
+
+    let col = document.createElement('div')
+            col.classList = 'col-md-3'
+
+            col.innerHTML= `<div class="card mb-4 shadow-sm">
+            <img class="bd-placeholder-img card-img-top"  src="${img}">
+           
+              
+            <div class="card-body">
+
+            <p class="card-text">
+            ${imgTitle}
+          </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" onclick="addToCart(event)">
+                    Add to cart
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary" onclick="skipBook(event)">
+                    skip
+                  </button>
+                </div>
+                <small class="text-muted"></small>
+              </div>
+            </div>
+          </div>`
+         Scart.appendChild(col)
+
+
 }
